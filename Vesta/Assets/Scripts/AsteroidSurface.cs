d98d7a7dd85bd18e317017293025855d169ed0d9
Mark Goadrich <goadrich@hendrix.edu>
2024-05-10 13:08:16 -0500

    Mines do not overlap.

--- Vesta/Assets/Scripts/AsteroidSurface.cs
+++ Vesta/Assets/Scripts/AsteroidSurface.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AsteroidSurface : MonoBehaviour
 {
@@ -32,7 +33,14 @@ public class AsteroidSurface : MonoBehaviour
     }
 
     void OnMouseDown() {
-        Instantiate(mine, transform);
+        Vector3 minepos = transform.position + (transform.forward * -0.5f);
+        Vector3Int mpi = new Vector3Int(Mathf.RoundToInt(minepos.x), 
+                                        Mathf.RoundToInt(minepos.y), 
+                                        Mathf.RoundToInt(minepos.z));
+        Debug.Log("tp: " + transform.position + ", forward: " + transform.forward + " minepos: " + mpi);
+        if (!GameManager.Instance.mines.ContainsKey(mpi)) {
+            GameManager.Instance.mines.Add(mpi, Instantiate(mine, transform));
+        }
     }
 
     void OnMouseExit() {
---
+++ Vesta/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    private static GameManager _instance;
+
+    public static GameManager Instance { get { return _instance; } }
+
+    public Dictionary<Vector3, GameObject> mines = new Dictionary<Vector3, GameObject>();
+
+    private void Awake() {
+        _instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
