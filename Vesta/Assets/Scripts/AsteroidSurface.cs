593e0e3603bea7b72c9a9864244587a320b314ad
Mark Goadrich <goadrich@hendrix.edu>
2024-05-31 14:44:45 -0500

    Abstracted Constructs, added Habitat

--- Vesta/Assets/Scripts/AsteroidSurface.cs
+++ Vesta/Assets/Scripts/AsteroidSurface.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -31,17 +32,7 @@ public class AsteroidSurface : MonoBehaviour
     }
 
     void OnMouseDown() {
-        if (GameManager.Instance.available["mine"] > 0) {
-            Vector3 minepos = transform.position + (transform.forward * -0.5f);
-            Vector3Int mpi = new Vector3Int(Mathf.RoundToInt(minepos.x), 
-                                            Mathf.RoundToInt(minepos.y), 
-                                            Mathf.RoundToInt(minepos.z));
-            Debug.Log("tp: " + transform.position + ", forward: " + transform.forward + " minepos: " + mpi);
-            if (!GameManager.Instance.mines.ContainsKey(mpi)) {
-                GameManager.Instance.mines.Add(mpi, Instantiate(GameManager.Instance.minePrefab, transform));
-            }
-            GameManager.Instance.available["mine"]--;
-        }
+        GameManager.Instance.AddBuilding(transform);
     }
 
     void OnMouseExit() {
--- Vesta/Assets/Scripts/GameManager.cs
+++ Vesta/Assets/Scripts/GameManager.cs
@@ -4,6 +4,13 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEditor.Build.Content;
 using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Construct {
+    public string name;
+    public GameObject prefab;
+}
 
 public class GameManager : MonoBehaviour
 {
@@ -12,7 +19,11 @@ public class GameManager : MonoBehaviour
 
     public static GameManager Instance { get { return _instance; } }
 
-    public Dictionary<Vector3, GameObject> mines = new Dictionary<Vector3, GameObject>();
+    public Dictionary<Vector3, GameObject> buildings = new Dictionary<Vector3, GameObject>();
+
+    public List<Construct> constructs = new List<Construct>();
+
+    public Dictionary<string, GameObject> constructDict = new Dictionary<string, GameObject>();
 
     public Dictionary<string, int> available;
 
@@ -20,10 +31,13 @@ public class GameManager : MonoBehaviour
 
     public GameObject availableContent;
 
-    public GameObject minePrefab;
+    public string selected;
 
     private void Awake() {
         _instance = this;
+        foreach (Construct construct in constructs) {
+            constructDict[construct.name] = construct.prefab;
+        }
     }
 
     // Start is called before the first frame update
@@ -34,6 +48,33 @@ public class GameManager : MonoBehaviour
         available["habitat"] = 2;
     }
 
+    void SetSelected(String which) {
+            selected = which;
+            print(which + " Selected");
+    }
+
+    public GameObject SelectedConstruct() {
+        if (selected != null && available.ContainsKey(selected) && available[selected] > 0) {
+            return constructDict[selected];
+        } else {
+            return null;
+        }
+    }
+
+    public void AddBuilding(Transform surface) {
+        GameObject construct = SelectedConstruct();
+        if (construct != null) {
+            Vector3 tempPosition = surface.position + (surface.forward * -0.5f);
+            Vector3Int constructPosPotential = new Vector3Int(Mathf.RoundToInt(tempPosition.x), 
+                                            Mathf.RoundToInt(tempPosition.y), 
+                                            Mathf.RoundToInt(tempPosition.z));
+            if (!buildings.ContainsKey(constructPosPotential)) {
+                buildings.Add(constructPosPotential, Instantiate(construct, surface));
+                available[selected]--;
+            }
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -42,10 +83,11 @@ public class GameManager : MonoBehaviour
             if (child == null) {
                 GameObject temp = Instantiate(availableButton, availableContent.transform);
                 temp.name = s;
-                temp.transform.Find("Label").GetComponent<TextMeshProUGUI>().text = s + ": " + available[s];
-            } else {
-                child.Find("Label").GetComponent<TextMeshProUGUI>().text = s + ": " + available[s];
-            }
-        }
+                child = temp.transform;
+                Button b = child.gameObject.GetComponent<Button>();
+                b.onClick.AddListener(delegate { SetSelected(child.gameObject.name); });
+            } 
+            child.Find("Label").GetComponent<TextMeshProUGUI>().text = s + ": " + available[s];
+         }
     }
 }
